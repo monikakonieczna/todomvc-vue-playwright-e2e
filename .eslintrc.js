9607461974c7e6354e91856f1497a1f535f0e47d
monika_konieczna <konieczna.monika01@gmail.com>
2026-01-19 23:17:22 +0100

    feat: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['tests/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+  ],
+};
